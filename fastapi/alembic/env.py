94d887a815c4746d805216f346d5de4cf80c5d1c
apayne185 <apayne2022@student.ie.edu>
2026-06-11 13:58:38 +0200

    alembic check added to ci

--- fastapi/alembic/env.py
+++ fastapi/alembic/env.py
@@ -33,6 +33,8 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        compare_type=True,
+        compare_server_default=True,
     )
     with context.begin_transaction():
         context.run_migrations()
@@ -46,7 +48,12 @@ def run_migrations_online() -> None:
         poolclass=pool.NullPool,
     )
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_type=True,
+            compare_server_default=True,
+        )
         with context.begin_transaction():
             context.run_migrations()
 
